7143210bc8217149f742bc91876cdb240c8ffc3e
ViniciusLima93 <vnlima9@gmail.com>
2023-02-20 11:49:41 -0400

    metodo validar cnpj PessoaJuridica

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using backend_vet.interfaces;
 
 
@@ -13,35 +14,78 @@ using backend_vet.interfaces;
 
 namespace backend_vet.Classes
 {
-    public class PessoaJuridica: Client, interfaces.IPessoaJuridica
+    public class PessoaJuridica: Client, IPessoaJuridica
     {
-        public string? cnpj {get;set;}
+        // public string? cnpj {get;set;}
 
-        public string? razaoSocial { get;set;}
+        // public string? razaoSocial { get;set;}
 
-        public override float CalcularImposto(float rendimento)
+        // public override float CalcularImposto(float rendimento)
+        // {
+
+        //     if (rendimento <=3000)
+        //   { 
+        //     float resultado;
+        //     resultado = ((float)(rendimento * 0.03f));
+        //   }else if (rendimento >= 3000 && rendimento <=6000) 
+        //   {
+        //     float resultado;
+        //     resultado = ((float)(rendimento * 0.05f));
+        //   }else if (rendimento >= 6000 && rendimento <=10000) 
+        //   {
+        //     float resultado;
+        //     resultado = ((float)(rendimento * 0.07f));
+        //   }else {
+        //     float resultado;
+        //     resultado = ((float)(rendimento * 0.09f));
+        //   }
+
+
+
+
+        //     throw new NotImplementedException();
+        // }
+
+        // bool IPessoaJuridica.ValidateCNPJ(string cnpj)
+        // {
+        //     throw new NotImplementedException();
+        // }
+      public string? Cnpj { get; set; }
+        
+        public string? razaoSocial { get; set; }
+
+        public override float CalcularImposto(float redimento)
         {
+            throw new NotImplementedException();
+        }
 
-            if (rendimento <=3000)
-          { 
-            float resultado;
-            resultado = ((float)(rendimento * 0.03f));
-          }else if (rendimento >= 3000 && rendimento <=6000) 
-          {
-            float resultado;
-            resultado = ((float)(rendimento * 0.05f));
-          }else if (rendimento >= 6000 && rendimento <=10000) 
-          {
-            float resultado;
-            resultado = ((float)(rendimento * 0.07f));
-          }else {
-            float resultado;
-            resultado = ((float)(rendimento * 0.09f));
-          }
+        //XX.XXX.XXX/0001-XX  ---  XXXXXXXX0001XX
+        public  bool ValidarCnpj(string cnpj)
+        {
+            if (Regex.IsMatch(cnpj, @"(^(\d{2}.\d{3}.\d{3}/\d{4}-\d{2})|(\d{14})$)"))
+            {
+                if (cnpj.Length == 18)
+                {
+                    if (cnpj.Substring(11, 4) == "0001")
+                    {
+                        return true;
+                    }
+                    
+                } else if (cnpj.Length == 14)
+                {
+                    if (cnpj.Substring(8, 4) == "0001")
+                    {
+                        return true;
+                    }
+                }
+            }
 
+            return false;
+   
+        }
 
-
-
+        float IPessoaJuridica.CalcularImposto(float rendimento)
+        {
             throw new NotImplementedException();
         }
 
--- Program.cs
+++ Program.cs
@@ -3,25 +3,29 @@
 
 using backend_vet.Classes;
 
-PessoaFisica novaPF = new PessoaFisica();
+// PessoaFisica novaPF = new PessoaFisica();
 
 
-novaPF.nome = "Vinicius";
-novaPF.cpf = "123456";
-novaPF.rendimento = 7000.5f;
+// novaPF.nome = "Vinicius";
+// novaPF.cpf = "123456";
+// novaPF.rendimento = 7000.5f;
 
 
 // Console.WriteLine(novaPF.nome);
 // Console.WriteLine(novaPF.cpf);
 
 
-float res = novaPF.CalcularImposto((float)novaPF.rendimento); 
-Console.WriteLine(res);
+// float res = novaPF.CalcularImposto((float)novaPF.rendimento); 
+// Console.WriteLine(res);
+
+// PessoaJuridica novaPJ = new PessoaJuridica();
+
+// float impostoPagar = novaPJ.CalcularImposto(120000.5f);
+// Console.WriteLine($"R$ {impostoPagar:0.00}");
+// Console.WriteLine($"R$ {impostoPagar.ToString("C")}");
 
 PessoaJuridica novaPJ = new PessoaJuridica();
 
-float impostoPagar = novaPJ.CalcularImposto(120000.5f);
-Console.WriteLine($"R$ {impostoPagar:0.00}");
-Console.WriteLine($"R$ {impostoPagar.ToString("C")}");
-
+bool res = novaPJ.ValidarCnpj((string)"12.345.678/0001-90");
+Console.WriteLine($"is a {res} as validate CNPJ");
 
